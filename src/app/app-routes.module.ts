ffef77deab6eee96bb22874b91cbf7dc4575bdca
Jirakrit <jayjirakrit@gmail.com>
2024-06-05 15:09:13 +0700

    init serichai-back-office-app

---
+++ src/app/app-routes.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Route } from '@angular/router';
+
+// const appRoutes: Route = [
+//   { path: 'login', component: ProductItemComponent }
+// ];
+
+@NgModule({
+  declarations: [],
+  imports: [
+  ]
+})
+export class AppRoutesModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutesModule } from './app-routes.module';
+import { ProductComponent } from './component/product/product.component';
+import { ProductItemComponent } from './component/product-item/product-item.component';
+import { SidebarComponent } from './component/core/sidebar/sidebar.component';
+import { HeaderComponent } from './component/core/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductItemComponent,
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutesModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
